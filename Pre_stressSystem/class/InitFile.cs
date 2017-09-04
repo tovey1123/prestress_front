5e4f31112491004f77d22f5a5335565ebf403604
Tovey <wzwfudan@126.com>
2017-09-04 21:35:44 +0800

    use RSA Encode

---
+++ Pre_stressSystem/class/InitFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+
+namespace Pre_stressSystem
+{
+    class InitFile
+{
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string defVal, StringBuilder retVal, int size, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int WritePrivateProfileString(string section, string key, string val, string filePath);
+
+
+        private string Path;
+        public InitFile(string path) {
+            this.Path = path;
+        }
+
+        //写INI文件
+        public void WriteValue(string section, string key, string value) {
+            WritePrivateProfileString(section, key, value, this.Path);
+        }
+        public string ReadValue(String section,string key)
+        {
+            //FileStream file = new FileStream(Path+"\\Info.ini", FileMode.OpenOrCreate);
+            StringBuilder temp = new StringBuilder(255);
+            int i = GetPrivateProfileString(section, key, "", temp, 255, this.Path);
+            return temp.ToString();
+
+        }
+
+
+    }
+}
--- Pre_stressSystem/loginPage.xaml.cs
+++ Pre_stressSystem/loginPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+
 
 namespace Pre_stressSystem
 {
@@ -19,6 +22,7 @@ namespace Pre_stressSystem
             this.m_parent = parent;
             InitializeComponent();
             this.textBox_username.Focus();
+            LoadIni();  //加载初始化信息--账号,密码,版本信息等
 
         }
 
@@ -61,7 +65,7 @@ namespace Pre_stressSystem
                     else
                     {
 
-                       // GlobalVariable.userName = textBox_username.Text;
+                        writeIntoIniFile();
                         m_parent.Content = new mainPage();
 
                     }
@@ -123,5 +127,75 @@ namespace Pre_stressSystem
             Window reg = new registWindow();
             reg.ShowDialog();
         }
+
+        //加载配置信息
+        private void LoadIni()
+        {
+            InitFile initFileReader = new InitFile(System.Environment.CurrentDirectory + "\\InitFile.ini");
+            this.textBox_username.Text = initFileReader.ReadValue("Login", "user");
+            Boolean isSelected = false;
+            try
+            {
+                isSelected = Convert.ToBoolean(initFileReader.ReadValue("Login", "rememberPwd"));
+            }catch(FormatException)
+            {
+                isSelected = false;
+            }
+            if (isSelected)
+            {
+                this.savePassword.IsChecked = true;
+                this.passwordBox.Password = RSADecrypt(initFileReader.ReadValue("Login", "passworld"));
+
+            }
+        }
+
+       //把信息写入INI文件
+        private void writeIntoIniFile()
+        {
+            InitFile initFileWriter = new InitFile(System.Environment.CurrentDirectory + "\\InitFile.ini");
+            initFileWriter.WriteValue("Login", "user", this.textBox_username.Text.PadLeft(4,'0'));
+            initFileWriter.WriteValue("Login", "passworld", RSAEncrypt(this.passwordBox.Password));
+            initFileWriter.WriteValue("Login", "rememberPwd", this.savePassword.IsChecked.ToString());
+        }
+    
+        //RSA加密
+        public static string RSAEncrypt(string content)
+        {
+           string publickey = @"<RSAKeyValue><Modulus>5m9m14XH3oqLJ8bNGw9e4rGpXpcktv9MSkHSVFVMjHbfv+
+                            SJ5v0ubqQxa5YjLN4vc49z7SVju8s0X4gZ6AzZTn06jzWOgyPRV54Q4I0DCYadWW4Ze3e
+                            +BOtwgVU1Og3qHKn8vygoj40J6U85Z/PTJu3hN1m75Zr195ju7g9v4Hk=
+                            </Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publickey);
+            byte[] cipherbytes;
+            cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(content), false);
+            return Convert.ToBase64String(cipherbytes);
+
+        }
+
+        public static string RSADecrypt(string content)
+        {
+            string privatekey = @"<RSAKeyValue><Modulus>5m9m14XH3oqLJ8bNGw9e4rGpXpcktv9MSkHSVFVMjHbfv
+                                +SJ5v0ubqQxa5YjLN4vc49z7SVju8s0X4gZ6AzZTn06jzWOgyPRV54Q4I0DCYadWW4Ze3e
+                                +BOtwgVU1Og3qHKn8vygoj40J6U85Z/PTJu3hN1m75Zr195ju7g9v4Hk=
+                                </Modulus>
+                                <Exponent>AQAB</Exponent>
+                                <P>/hf2dnK7rNfl3lbqghWcpFdu778hUpIEBixCDL5WiBtpkZdpSw90aERmHJYaW2RGvGRi6zSftLh00KHsPcNUMw==</P>
+                                <Q>6Cn/jOLrPapDTEp1Fkq+uz++1Do0eeX7HYqi9rY29CqShzCeI7LEYOoSwYuAJ3xA/DuCdQENPSoJ9KFbO4Wsow==</Q>
+                                <DP>ga1rHIJro8e/yhxjrKYo/nqc5ICQGhrpMNlPkD9n3CjZVPOISkWF7FzUHEzDANeJfkZhcZa21z24aG3rKo5Qnw==</DP>
+                                <DQ>MNGsCB8rYlMsRZ2ek2pyQwO7h/sZT8y5ilO9wu08Dwnot/7UMiOEQfDWstY3w5XQQHnvC9WFyCfP4h4QBissyw==</DQ>
+                                <InverseQ>EG02S7SADhH1EVT9DD0Z62Y0uY7gIYvxX/uq+IzKSCwB8M2G7Qv9xgZQaQlLpCaeKbux3Y59hHM+KpamGL19Kg==</InverseQ>
+                                <D>vmaYHEbPAgOJvaEXQl+t8DQKFT1fudEysTy31LTyXjGu6XiltXXHUuZaa2IPyHgBz0Nd7znwsW/S44iql0Fen1kzKioEL3svANui63O3o5xdDeExVM6zOf1wUUh/oldovPweChyoAdMtUzgvCbJk1sYDJf++Nr0FeNW1RB1XG30=</D>
+                                </RSAKeyValue>";
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            byte[] cipherbytes;
+            rsa.FromXmlString(privatekey);
+            cipherbytes = rsa.Decrypt(Convert.FromBase64String(content), false);
+
+            return Encoding.UTF8.GetString(cipherbytes);
+        }
+
+
+
     }
 }
